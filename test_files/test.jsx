6cc25057c9a80baa906a16b03568c702ec4d6fe5
Sean <seanpzk2@gmail.com>
2024-09-11 22:41:24 +0800

    Test lint error detection

--- test_files/test.jsx
+++ test_files/test.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1> {/* Missing closing tag for <h1> */}
+      <h1>Hello, world!</h1>
+      {/* Missing closing tag for <h1> */}
     </div>
   );
 }
