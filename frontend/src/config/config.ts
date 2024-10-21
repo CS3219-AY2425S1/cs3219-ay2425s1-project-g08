30213abb3eaa23d948b1bed5c58f2dda4f2bb70b
Sean <seanpzk2@gmail.com>
2024-10-21 21:25:46 +0800

    Update frontend GKE deployment

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,9 +1,9 @@
 const apiConfig = {
-    userServiceBaseUrl: process.env.USER_SERVICE_URL || "http://localhost:3001",
-    questionbankServiceBaseUrl: process.env.QB_SERVICE_URL || "http://localhost:8080",
-    profilePictureServiceBaseUrl: process.env.PP_SERVICE_URL || "http://localhost:8081",
-    matchWebsocketUrl: process.env.MATCH_WEBSOCKET_URL || "ws://localhost:8082",
-    matchExpressJsUrl: process.env.MATCH_EXPRESS_URL || "http://localhost:3000",
+    userServiceBaseUrl: import.meta.env.VITE_USER_SERVICE_URL ? "/usersvcapi" : "http://localhost:3001",
+    questionbankServiceBaseUrl: import.meta.env.VITE_QB_SERVICE_URL ? "/qbsvcapi" : "http://localhost:8080",
+    profilePictureServiceBaseUrl: import.meta.env.VITE_PP_SERVICE_URL ? "/ppsvcapi" : "http://localhost:8081",
+    matchWebsocketUrl: import.meta.env.VITE_MATCH_WEBSOCKET_URL ? "/matchwssvcapi" :"ws://localhost:8082",
+    matchExpressJsUrl: import.meta.env.VITE_MATCH_EXPRESS_URL ? "/matchexpresssvcapi" : "http://localhost:3000",
 };
 
 export default apiConfig;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,33 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    proxy: {
+      '/usersvcapi': {
+        target: 'http://user-service:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/usersvcapi/, '')
+      },
+      '/qbsvcapi' : {
+        target: 'http://questionbank-service:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/qbsvcapi/, '')
+      },
+      'ppsvcapi' : {
+        target: 'http://profilepicture-service:8081',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ppsvcapi/, '')
+      },
+      'matchwssvcapi' : {
+        target: 'ws://matching-service:8082',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/matchwssvcapi/, '')
+      },
+      'matchexpresssvcapi' : {
+        target: 'http://matching-service:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/matchexpresssvcapi/, '')
+      }
+    }
   },
   plugins: [react()],
   resolve: {
