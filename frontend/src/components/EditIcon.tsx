79f7ee8b4812352e5a75d5a62d20afeff6917fa3
Hmuu Myat Moe <hmuumyatmoe@gmail.com>
2024-10-25 21:35:55 +0800

    Minor UI fix: ensure edit icon do not disappear in minimised screens

--- frontend/src/components/EditIcon.tsx
+++ frontend/src/components/EditIcon.tsx
@@ -5,12 +5,12 @@ interface EditIconProps {
 export const EditIcon: React.FC<EditIconProps> = ({ openModal }) => {
     return (
         <div className=" flex items-center justify-center">
-                <button
-                    onClick={() => openModal()}
-                    className="bg-gray-500 rounded-lg p-4 text-2xl hover:bg-gray-300 h-auto w-auto"
-                >
-                <img src={editicon} alt="Edit" className="w-5 h-5"/>
-                </button>
+            <button
+                onClick={() => openModal()}
+                className="bg-gray-500 rounded-lg p-4 hover:bg-gray-300 h-auto w-auto"
+            >
+            <img src={editicon} alt="Edit" className="max-w-5 h-5"/>
+            </button>
         </div>
         
     );
