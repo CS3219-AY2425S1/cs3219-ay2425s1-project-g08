4a9bdfcba1f07134d4c1a2390345869f9a62d711
Sean <seanpzk2@gmail.com>
2024-11-01 11:56:04 +0800

    Attempt to fix lint error

--- history-service/src/main/java/com/example/history_service/HistoryServiceApplication.java
+++ history-service/src/main/java/com/example/history_service/HistoryServiceApplication.java
@@ -1,30 +1,1 @@
-package com.example.history_service;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-/**
- * The main entry point for the History service Spring Boot application.
- * <p>
- * This class contains the main method that launches the Spring Boot
- * application. The {@link SpringBootApplication} annotation is used
- * to mark this class as a Spring Boot application and to enable
- * autoconfiguration, component scanning, and configuration properties.
- *
- */
-@SpringBootApplication
-@EnableMongoRepositories
-public class HistoryServiceApplication {
-
-    /**
-     * The main method that serves as the entry point to the
-     * Spring Boot application.
-     *
-     * @param args command-line arguments passed to the application.
-     */
-    public static void main(final String... args) {
-        SpringApplication.run(HistoryServiceApplication.class, args);
-    }
-
-}
+package com.example.history_service;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;/** * The main entry point for the History service Spring Boot application. * <p> * This class contains the main method that launches the Spring Boot * application. The {@link SpringBootApplication} annotation is used * to mark this class as a Spring Boot application and to enable * autoconfiguration, component scanning, and configuration properties. * */@SpringBootApplication@EnableMongoRepositoriespublic class HistoryServiceApplication {    /**     * The main method that serves as the entry point to the     * Spring Boot application.     *     * @param args command-line arguments passed to the application.     */    public static void main(final String... args) {        SpringApplication.run(HistoryServiceApplication.class, args);    }}
