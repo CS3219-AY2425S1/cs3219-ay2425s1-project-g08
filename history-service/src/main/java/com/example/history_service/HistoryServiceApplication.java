6dde3a579354ba640da5d51af6d9a6b18d3aa7c3
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2024-11-01 11:32:36 +0800

    fix linter issue

--- history-service/src/main/java/com/example/history_service/HistoryServiceApplication.java
+++ history-service/src/main/java/com/example/history_service/HistoryServiceApplication.java
@@ -2,23 +2,28 @@ package com.example.history_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
- * The main entry point for the history service Spring Boot application.
+ * The main entry point for the History service Spring Boot application.
+ * <p>
+ * This class contains the main method that launches the Spring Boot
+ * application. The {@link SpringBootApplication} annotation is used
+ * to mark this class as a Spring Boot application and to enable
+ * autoconfiguration, component scanning, and configuration properties.
+ *
  */
 @SpringBootApplication
 @EnableMongoRepositories
 public class HistoryServiceApplication {
 
-	/**
-	 * The main method that is the entry point to the
-	 * Spring Boot application.
-	 *
-	 * @param args command-line arguments passed to the application.
-	 */
-	public static void main(final String... args) {
-		SpringApplication.run(HistoryServiceApplication.class, args);
-	}
+    /**
+     * The main method that serves as the entry point to the
+     * Spring Boot application.
+     *
+     * @param args command-line arguments passed to the application.
+     */
+    public static void main(final String... args) {
+        SpringApplication.run(HistoryServiceApplication.class, args);
+    }
 
 }
