a31688d7878f238b83f42218afedc0feff07390b
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2024-11-10 16:29:39 +0800

    Reverted connection hostname for redis

--- questionbank/src/main/java/com/example/questionbank/RedisConfig.java
+++ questionbank/src/main/java/com/example/questionbank/RedisConfig.java
@@ -6,11 +6,19 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+/**
+ * Configuration class for setting up Redis connection and RedisTemplate in the Spring application.
+ */
 @Configuration
 public class RedisConfig {
+    /**
+     * Creates a bean using Lettuce as the connection provider.
+     * This bean establishes a connection to the Redis server
+     * @return
+     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("redis-service", 6379);
+        return new LettuceConnectionFactory("redis", 6379);
     }
 
     @Bean
