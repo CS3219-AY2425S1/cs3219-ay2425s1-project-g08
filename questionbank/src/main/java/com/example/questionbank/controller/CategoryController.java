3dc4cf00fe3850c4e89e1e6b220ca171e1e4b2c7
Sean <seanpzk2@gmail.com>
2024-11-11 15:40:30 +0800

    Fix admin cannot perform crud on cloud

--- questionbank/src/main/java/com/example/questionbank/controller/CategoryController.java
+++ questionbank/src/main/java/com/example/questionbank/controller/CategoryController.java
@@ -17,10 +17,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "http://frontend-service:5173"
-})
+@CrossOrigin
 @RestController
 public class CategoryController {
 
